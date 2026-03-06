0bedb652ab8b5029c24d12d3f46e46974a85978e
David Sveningsson <ext@sidvind.com>
2026-03-06 20:12:37 +0100

    chore(deps): update @forsakringskassan/eslint-config monorepo to v13.5.0

--- src/parse-package-name.js
+++ src/parse-package-name.js
@@ -1,7 +1,7 @@
 /* eslint-disable-next-line sonarjs/slow-regex, sonarjs/no-empty-after-reluctant -- technical debt */
-const SCOPED_PACKAGE = /^(@[^/]+\/[^@/]+).*?$/;
+const SCOPED_PACKAGE = /^(@[^/]+\/[^/@]+).*?$/;
 /* eslint-disable-next-line sonarjs/slow-regex, sonarjs/no-empty-after-reluctant -- technical debt */
-const NON_SCOPED_PACKAGE = /^([^@/]+).*?$/;
+const NON_SCOPED_PACKAGE = /^([^/@]+).*?$/;
 
 export function getPackageNameFromPath(input) {
     const match = SCOPED_PACKAGE.exec(input) || NON_SCOPED_PACKAGE.exec(input);
