cec951128fe017d7f7337378b4671b1cd6559efa
David Sveningsson <ext@sidvind.com>
2026-03-22 18:38:10 +0100

    refactor: refactor to not use regexp to get package name

--- src/utils/get-package-name-from-path.ts
+++ src/utils/get-package-name-from-path.ts
@@ -1,17 +1,32 @@
-/* eslint-disable-next-line sonarjs/slow-regex, sonarjs/no-empty-after-reluctant -- technical debt */
-const SCOPED_PACKAGE = /^(@[^/]+\/[^/@]+).*?$/;
-/* eslint-disable-next-line sonarjs/slow-regex, sonarjs/no-empty-after-reluctant -- technical debt */
-const NON_SCOPED_PACKAGE = /^([^/@]+).*?$/;
+function isScoped(input: string): boolean {
+    return input.startsWith("@");
+}
 
+/**
+ * Get the package name of a given import path, i.e. it strips the subpath if present.
+ *
+ * For example:
+ *
+ * - `@org/pkg` becomes `@org/pkg`
+ * - `@org/pkg/subpath` becomes `@org/pkg`
+ * - `pkg` becomes `pkg`
+ * - `pkg/subpath` becomes `pkg`
+ *
+ * @internal
+ */
 export function getPackageNameFromPath(input?: string | null): string | null {
     if (!input) {
         return null;
     }
 
-    const match = SCOPED_PACKAGE.exec(input) ?? NON_SCOPED_PACKAGE.exec(input);
-    if (!match) {
+    if (input.startsWith(".") || input.startsWith("/")) {
         return null;
     }
 
-    return match[1];
+    const parts = input.split("/");
+    if (isScoped(input) && parts.length > 1) {
+        return `${parts[0]}/${parts[1]}`;
+    } else {
+        return parts[0];
+    }
 }
