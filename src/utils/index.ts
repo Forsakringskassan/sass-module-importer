ff4961c81e8fb1742a1868bd73595ea0858c52a5
David Sveningsson <ext@sidvind.com>
2026-03-22 18:50:03 +0100

    refactor: add `isWebpackPrefix()` helper

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { isErrnoError } from "./is-errno-error";
+export { isWebpackPrefix } from "./is-webpack-prefix";
 export { memoize } from "./memoize";
 export { readJsonFile } from "./read-json-file";
